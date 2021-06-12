2451b0faa156a39ca4940334fb41fede3cb57454
AnnaStoyano <50628589+AnnaStoyano@users.noreply.github.com>
2021-06-12 21:39:00 +0300

    V5.0 react (#8)
    * Start refactoring code to React
    
    * add AllFilters Component and change structure
    
    * Add Sorting Components and remove framework
    
    * Change sorting names Ascending & Descending #4
    
    * Add age on card display #5 and fix some sorting bugs
    
    * fix bugs and change output #6
    
    * Fix sorting bug

--- src/data/helper.js
+++ src/data/helper.js
@@ -16,24 +16,24 @@ export function switchInputs(event, defaultValue) {
   return defaultValue;
 }
 
-export function compareByAge(character1Age, character2Age, currentAge) {
-  if (currentAge === 'Youngest first') {
+export function compareByAge(character1Age, character2Age, currentAge, AGE_FLAG) {
+  if (currentAge === AGE_FLAG) {
     return character2Age.getTime() - character1Age.getTime();
   }
-
   return character1Age - character2Age;
 }
 
 export function getDatefromValue(value) {
   if (typeof value == 'string') {
     const [day, month, year] = value.split('-');
-    return new Date(year, month, day);
+    return new Date(year, month - 1, day);
   } else if (typeof value == 'number') {
     return new Date(value);
   }
 }
 
 export function getCharacterAge(str) {
+  const START_YEAR = 1969;
   const ageDate = new Date(Date.now() - getDatefromValue(str));
-  return ageDate.getFullYear() - 1969;
+  return ageDate.getFullYear() - START_YEAR;
 }
